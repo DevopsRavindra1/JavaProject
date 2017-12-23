b540731fbaff8f26a8b0fbdc2b95790ae3ec0fbf
GitClas <gitfirstclass@gmail.com>
2017-12-23 11:57:09 +0530

    two files

---
+++ simpleintrest1.java
@@ -0,0 +1,16 @@
+class simpleintrest1
+{
+public static void main(String [] args)
+{
+int p,r,t,omi,toti,total;
+p=5000;
+r=2;
+t=15;
+omi=5000*2/100;
+System.out.println("The One month Intrestis "+omi);
+toti=omi*t;
+System.out.println("The total Intrest is "+toti);
+total=p+toti;
+System.out.println("The Total Payble Amount is "+total);
+}
+}
